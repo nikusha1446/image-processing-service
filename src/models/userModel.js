b5316d862e028ced2e995f0ba5d9fee686346fc0
Nikusha <nikusha1446@gmail.com>
2025-10-17 19:30:13 +0400

    feat: create User model with password hashing utilities

---
+++ src/models/userModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { comparePassword, hashPassword } from '../utils/password.js';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: true,
+      trim: true,
+      minlength: [3, 'Username must be at least 3 characters long'],
+      maxlength: [30, 'Username must be less than 30 characters'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    this.password = await hashPassword(this.password);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await comparePassword(password, this.password);
+};
+
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
